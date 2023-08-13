ad37a88b13e79919c539ffdbe4cbc4b0803820d1
knight <karvin_jimenez@hotmail.com>
2023-08-13 11:49:15 -0400

    Project started.

---
+++ core/src/main/java/knight/arkham/Asteroid.java
@@ -0,0 +1,11 @@
+package knight.arkham;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Asteroid extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
